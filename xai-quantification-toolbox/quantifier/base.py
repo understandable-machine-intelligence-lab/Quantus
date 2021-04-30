d5cbd4c2b0a99c312ff3c2ffa9827cb5d99921da
3-nan <franz.motzkus@aol.de>
2021-04-30 16:57:24 +0200

    Suggestions for Measure and Quantifier

--- xai-quantification-toolbox/quantifier/base.py
+++ xai-quantification-toolbox/quantifier/base.py
@@ -2,6 +2,8 @@
 from typing import Union, List
 import tensorflow as tf
 import torch
+import numpy as np
+
 from ..measures.base import Measure
 
 
@@ -23,6 +25,49 @@ class Quantifier:
             for e in explanations:
                 for m in self.measures:
                     self. = m.fit(i, e)
+
+    @staticmethod
+    def get_explanations(self):
+        pass
+
+
+class ModularQuantifier:
+    """
+        This class is where evaluation specified and where it is run against some data, explanations and model.
+        Here we specify how things are run (local, #gpus) and where the output is stored.
+        """
+
+    def __init__(self,
+                 measures: List[Measure]):
+        self.measures = measures
+
+    def fit(self, inputs, targets, model, attributions=None, save_to_file=False):
+        if attributions is None:
+            get_explanations(inputs, targets)
+
+        if isinstance(inputs, np.ndarray):
+            for measure in self.measures:
+                results = measure(model, inputs, targets, attributions)
+                self.__save_results(measure, results, save_to_file=save_to_file)
+
+        else:
+            try:
+                for batch in inputs:
+                    # extract data and handles(fname) from dataloader
+                    data, handles = extract_data(batch)
+                    # load attributions
+                    attributions = get_attributions(data, targets, handles)
+                    # compute measures
+                    for measure in self.measures:
+                        results = measure(model, data, targets, attributions)
+                        self.__save_results(measure, results, save_to_file=save_to_file)
+
+            except TypeError:
+                print("{} is not iterable.".format(inputs))
+
+    def __save_results(self, measure, results, save_to_file=False):
+        """ Save Measure results to internal representation. """
+        raise NotImplementedError()
 
     @staticmethod
     def get_explanations(self):
