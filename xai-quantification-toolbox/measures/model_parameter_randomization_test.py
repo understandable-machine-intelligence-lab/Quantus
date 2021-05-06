b895be8b619b0a5395369665bfa0b289747aec7f
3-nan <franz.motzkus@aol.de>
2021-05-06 14:56:13 +0200

    added model_parameter_randomization_test.py problems: - attribution linkage - one target / multiple targets (Kohlbrenner score differs) - how/where to store/provide results

---
+++ xai-quantification-toolbox/measures/model_parameter_randomization_test.py
@@ -0,0 +1,51 @@
+import numpy as np
+from typing import Union
+
+from .base import Measure
+
+
+class ModelParameterRandomizationTest(Measure):
+    """ Implements the Model Parameter Randomization Method as described in
+    Adebayo et. al., 2018, Sanity Checks for Saliency Maps
+    """
+
+    def __init__(self, **params):
+        self.params = params
+        self.distance_measure = params.get("distance_measure", "spearman")
+        self.layer_order = params.get("layer_order", "independent")
+
+        super(ModelParameterRandomizationTest, self).__init__()
+
+    def __call__(self,
+                 model,
+                 inputs: np.array,
+                 targets: Union[np.array, int, None],
+                 attributions: Union[np.array, None],
+                 **kwargs):
+
+        results = []
+
+        for layer in get_layers(model, order=(self.layer_order == "top_down")):
+
+            layer_results = []
+
+            model = randomize_layer(model, layer, independent=(self.layer_order == "independent"))
+
+            for s, sample in enumerate(inputs):
+
+                original_attribution = attributions[s]
+
+                modified_attribution = compute_attribution(model, ...)
+
+                # normalize attributions
+                original_attribution /= np.max(np.abs(original_attribution))
+                modified_attribution /= np.max(np.abs(modified_attribution))
+
+                # compute distance measure / ToDo: ensure computation of multiple distance measures at once
+                distance = self.distance_measure(modified_attribution, original_attribution)
+
+                layer_results.append(distance)
+
+            results.append(layer_results)   # how to save results ??
+
+        return results
