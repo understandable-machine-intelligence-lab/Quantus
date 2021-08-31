d4ad536d5fab99b49bbca0be49e4350854263d50
annahedstroem <anna.hedstroem@tu-berlin.de>
2021-08-31 15:16:49 +0200

    commit test branch

--- quantus/metrics/faithfulness_metrics.py
+++ quantus/metrics/faithfulness_metrics.py
@@ -1300,6 +1300,7 @@ class SensitivityN(Metric):
             for i_ix, a_ix in enumerate(a_indices[:: self.features_in_step]):
 
                 if i_ix <= self.max_features:
+
                     # Perturb input by indices of attributions.
                     a_ix = a_indices[
                         (self.features_in_step * i_ix) : (
