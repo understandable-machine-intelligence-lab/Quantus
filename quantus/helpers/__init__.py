36a55dc5430e2cbbcb04b3050cabaecc87050130
annahedstroem <anna.hedstroem@tu-berlin.de>
2021-09-24 13:55:34 +0200

    update voronoy and all metrics tutorials

--- quantus/helpers/__init__.py
+++ quantus/helpers/__init__.py
@@ -2,9 +2,9 @@ from .asserts import *
 from .constants import *
 from .utils import *
 from .plotting import *
+from .normalize_func import *
 from .perturb_func import *
 from .norm_func import *
 from .similar_func import *
 from .explanation_func import *
 from .local_func import *
-from .normalize_func import *
