c10eeae1b69ddb9554cfc65062ba72ff72bdb82f
annahedstroem <anna.hedstroem@tu-berlin.de>
2021-10-15 09:26:51 +0200

    tests - init

--- quantus/tests/test_metrics.py
+++ quantus/tests/test_metrics.py
@@ -5,5 +5,10 @@ import torch
 img_size = 12
 
 # Define test scenarios.
-s_batch = torch.zeros((1, 1, img_size, img_size))
-s_batch = torch.zeros((1, 1, img_size, img_size))
+S_BATCH = torch.zeros((1, 1, img_size, img_size))
+S_BATCH = torch.zeros((1, 1, img_size, img_size))
+A_BATCH_1 = torch.zeros((1, 1, img_size, img_size))
+
+@pytest.mark.localisation
+def test_pointing_game():
+    pass
