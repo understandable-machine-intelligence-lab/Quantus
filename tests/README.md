14f57f233eb89947dcb7730bc94fff4d11a8e0f4
annahedstroem <anna.hedstroem@tu-berlin.de>
2021-11-17 21:13:27 +0100

    pytests for all quantus metrics and example inserted in call docstrings

---
+++ tests/README.md
@@ -0,0 +1,7 @@
+### How to run tests
+
+All tests
+> pytest
+
+A group of tests e.g., localisation
+> pytest -m localisation -s
---
+++ tests/helpers/test_normalise_func.py
@@ -0,0 +1,5 @@
+import pytest
+from typing import Union
+from pytest_lazyfixture import lazy_fixture
+from ..fixtures import *
+from ...quantus.helpers import *
