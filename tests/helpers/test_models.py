800423cf3d0efdc67b51904cf7778a0a25c45192
annahedstroem <anna.hedstroem@tu-berlin.de>
2021-11-26 16:38:47 +0100

    pytests - helpers implementation

--- tests/helpers/test_models.py
+++ tests/helpers/test_models.py
@@ -1,5 +1,1 @@
-import pytest
-from typing import Union
-from pytest_lazyfixture import lazy_fixture
-from ..fixtures import *
-from ...quantus.helpers import *
+"""No identified need to test plotting functionality."""
--- tests/helpers/test_normalise_func.py
+++ tests/helpers/test_normalise_func.py
@@ -3,3 +3,13 @@ from typing import Union
 from pytest_lazyfixture import lazy_fixture
 from ..fixtures import *
 from ...quantus.helpers import *
+
+
+@pytest.mark.normalise_func
+@pytest.mark.parametrize("data,params,expected", [(lazy_fixture("atts_normalise"), {},
+                                                   np.array([0.2,  0.4,  0.6,  0.8,  0.8,  1.0, -0.2]))])
+def test_normalise_by_max(data: dict,
+                          params: dict,
+                          expected: Union[float, dict]):
+    out = normalise_by_max(a=data)
+    assert all(out == expected), "Test failed."
