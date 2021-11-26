f5a4cf5603c11153b05109143fd671ed2a974377
annahedstroem <anna.hedstroem@tu-berlin.de>
2021-11-26 16:38:47 +0100

    pytests - helpers implementation

--- tests/helpers/test_norm_func.py
+++ tests/helpers/test_norm_func.py
@@ -3,3 +3,38 @@ from typing import Union
 from pytest_lazyfixture import lazy_fixture
 from ..fixtures import *
 from ...quantus.helpers import *
+
+
+@pytest.mark.norm_func
+@pytest.mark.parametrize("data,params,expected", [(lazy_fixture("atts_norm_ones"), {}, 3.1622776601683795),
+                                                  (lazy_fixture("atts_norm_fill"), {}, 11.40175425099138)])
+def test_fro_norm(data: dict,
+                  params: dict,
+                  expected: Union[float, dict]):
+    out = fro_norm(a=data)
+    assert out == expected, "Test failed."
+
+
+@pytest.mark.norm_func
+@pytest.mark.parametrize("data,params,expected", [(lazy_fixture("atts_norm_ones"), {}, 1.0),
+                                                  (lazy_fixture("atts_norm_fill"), {}, 10)])
+def test_linf_norm(data: dict,
+                   params: dict,
+                   expected: Union[float, dict]):
+    out = linf_norm(a=data)
+    assert out == expected, "Test failed."
+
+'''
+def test_l2_norm(data: dict,
+                          params: dict,
+                          expected: Union[float, dict]):
+    out = linf_norm(a=data)
+    assert out == expected, "Test failed."
+
+
+def test_l1_norm(data: dict,
+                          params: dict,
+                          expected: Union[float, dict]):
+    out = linf_norm(a=data)
+    assert out == expected, "Test failed."
+'''
--- tests/metrics/test_randomisation_metrics.py
+++ tests/metrics/test_randomisation_metrics.py
@@ -23,10 +23,10 @@ def test_model_parameter_randomsiation(data: dict, params: dict, expected: Union
 @pytest.mark.parametrize("data,params,expected", [(lazy_fixture("almost_uniform"), {"normalise": True}, 1.0),
                                                   (lazy_fixture("almost_uniform"), {"normalise": False}, 1.0)])
 def test_non_random_logit(data: dict, params: dict, expected: Union[float, dict]):
-    scores = RandomLogit(**params)(model=None,
-                                  x_batch=data["x_batch"],
-                                  y_batch=data["y_batch"],
-                                  a_batch=data["a_batch"])
+    scores = RandomLogit(**params)(model=model,
+                                   x_batch=data["x_batch"],
+                                   y_batch=data["y_batch"],
+                                   a_batch=data["a_batch"])
     if isinstance(expected, float):
         assert all(s == expected for s in scores), "Test failed."
     else:
