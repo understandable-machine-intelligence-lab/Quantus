19230f5cd4a632a4a254d6b03ccf89cacae50dca
dilyarabareeva <dilyara.bareeva@tu-berlin.de>
2022-01-26 10:28:10 +0100

    add comments, small changes to explanation_func.py

--- quantus/helpers/pytorch_model.py
+++ quantus/helpers/pytorch_model.py
@@ -7,10 +7,13 @@ import numpy as np
 
 
 class PyTorchModel(ModelInterface):
+    """Interface for torch models."""
+
     def __init__(self, model, channel_first):
         super().__init__(model, channel_first)
 
     def predict(self, x, softmax_act=False, **kwargs):
+        """Predict on the given input."""
         if self.model.training:
             raise AttributeError("Torch model needs to be in the evaluation mode.")
 
@@ -27,21 +30,30 @@ class PyTorchModel(ModelInterface):
             return pred.numpy()
 
     def shape_input(self, x, img_size, nr_channels):
+        """Reshape input into model expected input."""
         x = x.reshape(1, nr_channels, img_size, img_size)
         if self.channel_first:
             return x
         raise ValueError("Channel first order expected for a torch model.")
 
     def get_model(self):
+        """Get the original torch/tf model."""
         return self.model
 
     def state_dict(self):
+        """Get a dictionary of the model's learnable parameters."""
         return self.model.state_dict()
 
     def load_state_dict(self, original_parameters):
+        """Set model's learnable parameters."""
         self.model.load_state_dict(original_parameters)
 
     def get_random_layer_generator(self, order: str = "top_down"):
+        """
+        In every iteration yields a copy of the model with one additional layer's parameters randomized.
+        Set order to top_down for cascading randomization.
+        Set order to independent for independent randomization.
+        """
         original_parameters = self.state_dict()
         random_layer_model = deepcopy(self.model)
 
