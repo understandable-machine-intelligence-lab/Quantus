503af020155a11c3da266488df0eafdebb380e84
Ferran Parés <ferran.pares.pont@gmail.com>
2022-06-07 16:52:08 +0200

    adding a LeNet with adaptive pooling

--- quantus/helpers/models.py
+++ quantus/helpers/models.py
@@ -33,6 +33,34 @@ if util.find_spec("torch"):
             x = self.fc_3(x)
             return x
 
+    class LeNetAdaptivePooling(torch.nn.Module):
+        """Network architecture from with adaptive pooling: https://github.com/ChawDoe/LeNet5-MNIST-PyTorch."""
+
+        def __init__(self):
+            super().__init__()
+            self.conv_1 = torch.nn.Conv2d(1, 6, 5)
+            self.pool_1 = torch.nn.MaxPool2d(2, 2)
+            self.relu_1 = torch.nn.ReLU()
+            self.conv_2 = torch.nn.Conv2d(6, 16, 5)
+            self.pool_2 = torch.nn.MaxPool2d(2, 2)
+            self.relu_2 = torch.nn.ReLU()
+            self.avg_pooling = torch.nn.AdaptiveAvgPool2d((4, 4))
+            self.fc_1 = torch.nn.Linear(256, 120)
+            self.relu_3 = torch.nn.ReLU()
+            self.fc_2 = torch.nn.Linear(120, 84)
+            self.relu_4 = torch.nn.ReLU()
+            self.fc_3 = torch.nn.Linear(84, 10)
+
+        def forward(self, x):
+            x = self.pool_1(self.relu_1(self.conv_1(x)))
+            x = self.pool_2(self.relu_2(self.conv_2(x)))
+            x = self.avg_pooling(x)
+            x = x.view(x.shape[0], -1)
+            x = self.relu_3(self.fc_1(x))
+            x = self.relu_4(self.fc_2(x))
+            x = self.fc_3(x)
+            return x
+
     class ConvNet1D(torch.nn.Module):
         """1D-convolutional architecture inspired from LeNet."""
 
