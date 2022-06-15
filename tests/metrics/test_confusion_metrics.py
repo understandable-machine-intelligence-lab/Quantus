9713e06b9972779d67840e70315a33a67e257e3b
Ferran Parés <ferran.pares.pont@gmail.com>
2022-06-15 13:04:20 +0200

    WIP adding extensive test cases

--- tests/metrics/test_confusion_metrics.py
+++ tests/metrics/test_confusion_metrics.py
@@ -1,3 +1,8 @@
+from glob import glob
+import torch
+import numpy as np
+from PIL import Image
+
 import pytest
 from pytest_lazyfixture import lazy_fixture
 
@@ -6,6 +11,10 @@ from ...quantus.metrics import *
 from ...quantus.helpers import *
 from ...quantus.helpers import perturb_func
 from ...quantus.helpers.explanation_func import explain
+
+from tensorflow.keras.datasets import cifar10
+from torchvision.models.resnet import resnet18
+from torchvision import transforms
 
 @pytest.fixture
 def load_artificial_attribution():
@@ -45,7 +54,79 @@ def load_mnist_mosaics():
     ).numpy()
     mosaics_returns = mosaic_creation(images=x_batch, labels=y_batch, mosaics_per_class=10, seed=777)
     all_mosaics, mosaic_indices_list, mosaic_labels_list, p_batch_list, target_list = mosaics_returns
+    return {
+        "x_batch": all_mosaics,
+        "y_batch": target_list,
+        "p_batch": p_batch_list,
+    }
 
+
+@pytest.fixture()
+def load_cifar10_adaptive_lenet_model():
+    """Load a pre-trained LeNet classification model (architecture at quantus/helpers/models)."""
+    model = LeNetAdaptivePooling(input_shape=(3, 32, 32))
+    model.load_state_dict(
+        torch.load("tutorials/assets/cifar10", map_location="cpu", pickle_module=pickle)
+    )
+    return model
+
+
+@pytest.fixture
+def load_cifar10_mosaics():
+    """Load a batch of Cifar10 and build mosaics from them"""
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    x_batch = torch.as_tensor(
+        x_train[:124, ...].reshape(124, 3, 32, 32),
+        dtype=torch.float,
+    ).numpy()
+    y_batch = torch.as_tensor(
+        y_train[:124].reshape(124), dtype=torch.int64
+    ).numpy()
+    mosaics_returns = mosaic_creation(images=x_batch, labels=y_batch, mosaics_per_class=10, seed=777)
+    all_mosaics, mosaic_indices_list, mosaic_labels_list, p_batch_list, target_list = mosaics_returns
+    return {
+        "x_batch": all_mosaics,
+        "y_batch": target_list,
+        "p_batch": p_batch_list,
+    }
+
+
+@pytest.fixture()
+def load_imagenet_resnet18_model():
+    """Load a pre-trained ResNet18 classification model (architecture at quantus/helpers/models)."""
+    model = resnet18(pretrained=True)
+    return model
+
+
+@pytest.fixture()
+def load_imagenet_mosaics():
+    """Load a batch of ImageNet and build mosaics from it."""
+
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    with open("tutorials/assets/imagenet_images/imagenet1000_clsid_to_labels.txt") as mapper_file:
+        label_map = {line.split(':')[0]: idx for idx, line in enumerate(mapper_file.readlines())}
+
+    x_validation, y_validation = [], []
+    for image_path in glob("tutorials/assets/imagenet_images/images/*/*"):
+        input_img = Image.open(image_path).convert('RGB')
+        input_tensor = preprocess(input_img)
+        input_batch = input_tensor.unsqueeze(0)
+        x_validation.append(input_batch)
+
+        label_str = os.path.basename(os.path.dirname(image_path))
+        label_int = label_map[label_str]
+        y_validation.append(label_int)
+
+    x_batch = torch.concat(x_validation).numpy()
+    y_batch = np.asarray(y_validation)
+
+    mosaics_returns = mosaic_creation(images=x_batch, labels=y_batch, mosaics_per_class=10, seed=777)
+    all_mosaics, mosaic_indices_list, mosaic_labels_list, p_batch_list, target_list = mosaics_returns
     return {
         "x_batch": all_mosaics,
         "y_batch": target_list,
@@ -150,6 +231,37 @@ def load_mnist_mosaics():
                     "method": "GradCam",
                     "gc_layer": "model._modules.get('conv_2')",
                     "abs": True,
+                    "interpolate": (56, 56),
+                    "disable_warnings": False,
+                    "display_progressbar": False,
+                },
+                None,
+        ),
+        (
+                lazy_fixture("load_cifar10_adaptive_lenet_model"),
+                lazy_fixture("load_cifar10_mosaics"),
+                None,
+                {
+                    "explain_func": explain,
+                    "method": "GradCam",
+                    "gc_layer": "model._modules.get('conv_2')",
+                    "abs": True,
+                    "interpolate": (64, 64),
+                    "disable_warnings": False,
+                    "display_progressbar": False,
+                },
+                None,
+        ),
+        (
+                lazy_fixture("load_imagenet_resnet18_model"),
+                lazy_fixture("load_imagenet_mosaics"),
+                None,
+                {
+                    "explain_func": explain,
+                    "method": "GradCam",
+                    "gc_layer": "model._modules.get('layer4')[-1]",
+                    "abs": True,
+                    "interpolate": (448, 448),
                     "disable_warnings": False,
                     "display_progressbar": False,
                 },
@@ -239,15 +351,30 @@ def test_focus(
             )
         return
 
-    scores = metric(
-        model=model,
-        x_batch=x_batch,
-        y_batch=y_batch,
-        a_batch=a_batch,
-        p_batch=p_batch,
-        **params,
-    )
+    while len(p_batch) > 0:
+        if x_batch is not None:
+            x_minibatch, x_batch = x_batch[:10], x_batch[10:]
+        else:
+            x_minibatch = None
+        if y_batch is not None:
+            y_minibatch, y_batch = y_batch[:10], y_batch[10:]
+        else:
+            y_minibatch = None
+        if a_batch is not None:
+            a_minibatch, a_batch = a_batch[:10], a_batch[10:]
+        else:
+            a_minibatch = None
+        p_minibatch, p_batch = p_batch[:10], p_batch[10:]
+        metric(
+            model=model,
+            x_batch=x_minibatch,
+            y_batch=y_minibatch,
+            a_batch=a_minibatch,
+            p_batch=p_minibatch,
+            **params,
+        )
 
+    scores = metric.last_results
     assert len(scores) == len(p_batch), "Test failed."
     assert all([0 <= score <= 1 for score in scores]), "Test failed."
     if expected and "value" in expected:
