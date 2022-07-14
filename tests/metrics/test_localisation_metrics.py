d6211ade64e8cf321a0398d0867753c7c420eca2
annahedstroem <anna.hedstroem@tu-berlin.de>
2022-07-14 19:38:23 +0200

    Most Focus-merge fixes

--- tests/metrics/test_localisation_metrics.py
+++ tests/metrics/test_localisation_metrics.py
@@ -330,7 +330,12 @@ def load_artificial_attribution():
     ones = np.ones((1, 28, 28))
     mosaics_list = []
     images = [zeros, ones]
-    indices_list = [tuple([0,0,1,1]), tuple([1,1,0,0]), tuple([0,1,0,1]), tuple([1,0,1,0])]
+    indices_list = [
+        tuple([0, 0, 1, 1]),
+        tuple([1, 1, 0, 0]),
+        tuple([0, 1, 0, 1]),
+        tuple([1, 0, 1, 0]),
+    ]
     for indices in indices_list:
         first_row = np.concatenate((images[indices[0]], images[indices[1]]), axis=1)
         second_row = np.concatenate((images[indices[2]], images[indices[3]]), axis=1)
@@ -359,8 +364,16 @@ def load_mnist_mosaics():
     y_batch = torch.as_tensor(
         np.loadtxt("tutorials/assets/mnist_y"), dtype=torch.int64
     ).numpy()
-    mosaics_returns = mosaic_creation(images=x_batch, labels=y_batch, mosaics_per_class=10, seed=777)
-    all_mosaics, mosaic_indices_list, mosaic_labels_list, p_batch_list, target_list = mosaics_returns
+    mosaics_returns = mosaic_creation(
+        images=x_batch, labels=y_batch, mosaics_per_class=10, seed=777
+    )
+    (
+        all_mosaics,
+        mosaic_indices_list,
+        mosaic_labels_list,
+        p_batch_list,
+        target_list,
+    ) = mosaics_returns
     return {
         "x_batch": all_mosaics,
         "y_batch": target_list,
@@ -386,11 +399,17 @@ def load_cifar10_mosaics():
         x_train[:124, ...].reshape(124, 3, 32, 32),
         dtype=torch.float,
     ).numpy()
-    y_batch = torch.as_tensor(
-        y_train[:124].reshape(124), dtype=torch.int64
-    ).numpy()
-    mosaics_returns = mosaic_creation(images=x_batch, labels=y_batch, mosaics_per_class=10, seed=777)
-    all_mosaics, mosaic_indices_list, mosaic_labels_list, p_batch_list, target_list = mosaics_returns
+    y_batch = torch.as_tensor(y_train[:124].reshape(124), dtype=torch.int64).numpy()
+    mosaics_returns = mosaic_creation(
+        images=x_batch, labels=y_batch, mosaics_per_class=10, seed=777
+    )
+    (
+        all_mosaics,
+        mosaic_indices_list,
+        mosaic_labels_list,
+        p_batch_list,
+        target_list,
+    ) = mosaics_returns
     return {
         "x_batch": all_mosaics,
         "y_batch": target_list,
@@ -1306,234 +1325,239 @@ def test_attribution_localisation(
         assert all(s < expected["max"] for s in scores), "Test failed."
 
 
-
-
-
-
-
 @pytest.mark.localisation
 @pytest.mark.parametrize(
     "model,mosaic_data,a_batch,params,expected",
     [
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "Gradient",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "Gradient",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "GradientShap",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "GradientShap",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "GradientShap",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "GradientShap",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "IntegratedGradients",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "IntegratedGradients",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "InputXGradient",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "InputXGradient",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "Saliency",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "Saliency",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "InputXGradient",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "InputXGradient",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "Saliency",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "Saliency",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "Occlusion",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "Occlusion",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "FeatureAblation",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "FeatureAblation",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "GradCam",
-                    "gc_layer": "model._modules.get('conv_2')",
-                    "pos_only": True,
-                    "interpolate": (56, 56),
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "GradCam",
+                "gc_layer": "model._modules.get('conv_2')",
+                "pos_only": True,
+                "interpolate": (56, 56),
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_cifar10_adaptive_lenet_model"),
-                lazy_fixture("load_cifar10_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "GradCam",
-                    "gc_layer": "model._modules.get('conv_2')",
-                    "pos_only": True,
-                    "interpolate": (64, 64),
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_cifar10_adaptive_lenet_model"),
+            lazy_fixture("load_cifar10_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "GradCam",
+                "gc_layer": "model._modules.get('conv_2')",
+                "pos_only": True,
+                "interpolate": (64, 64),
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                lazy_fixture("load_mnist_mosaics"),
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "Control Var. Sobel Filter",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                None,
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            lazy_fixture("load_mnist_mosaics"),
+            None,
+            {
+                "explain_func": explain,
+                "method": "Control Var. Sobel Filter",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            None,
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                {
-                    "x_batch": None,
-                    "y_batch": None,
-                    "p_batch": None,
-                },
-                None,
-                {
-                    "explain_func": explain,
-                    "method": "Gradient",
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                {'exception': ValueError},
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            {
+                "x_batch": None,
+                "y_batch": None,
+                "p_batch": None,
+            },
+            None,
+            {
+                "explain_func": explain,
+                "method": "Gradient",
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            {"exception": ValueError},
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                {
-                    "x_batch": np.ones((4, 1, 56, 56)),
-                    "y_batch": np.ones(4),
-                    "p_batch": [tuple([0,0,1,1]), tuple([1,1,0,0]), tuple([0,1,0,1]), tuple([1,0,1,0])],
-                },
-                lazy_fixture("load_artificial_attribution"),
-                {
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                {"value": 1},
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            {
+                "x_batch": np.ones((4, 1, 56, 56)),
+                "y_batch": np.ones(4),
+                "p_batch": [
+                    tuple([0, 0, 1, 1]),
+                    tuple([1, 1, 0, 0]),
+                    tuple([0, 1, 0, 1]),
+                    tuple([1, 0, 1, 0]),
+                ],
+            },
+            lazy_fixture("load_artificial_attribution"),
+            {
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            {"value": 1},
         ),
         (
-                lazy_fixture("load_mnist_adaptive_lenet_model"),
-                {
-                    "x_batch": np.ones((4, 1, 56, 56)),
-                    "y_batch": np.ones(4),
-                    "p_batch": [tuple([1, 1, 0, 0]), tuple([0, 0, 1, 1]), tuple([1, 0, 1, 0]), tuple([0, 1, 0, 1])],
-                },
-                lazy_fixture("load_artificial_attribution"),
-                {
-                    "disable_warnings": False,
-                    "display_progressbar": False,
-                },
-                {"value": 0},
+            lazy_fixture("load_mnist_adaptive_lenet_model"),
+            {
+                "x_batch": np.ones((4, 1, 56, 56)),
+                "y_batch": np.ones(4),
+                "p_batch": [
+                    tuple([1, 1, 0, 0]),
+                    tuple([0, 0, 1, 1]),
+                    tuple([1, 0, 1, 0]),
+                    tuple([0, 1, 0, 1]),
+                ],
+            },
+            lazy_fixture("load_artificial_attribution"),
+            {
+                "disable_warnings": False,
+                "display_progressbar": False,
+            },
+            {"value": 0},
         ),
     ],
 )
 def test_focus(
-        model: Optional[ModelInterface],
-        mosaic_data: Dict[str, Union[np.ndarray, list]],
-        a_batch: Optional[np.ndarray],
-        params: dict,
-        expected: Optional[dict],
+    model: Optional[ModelInterface],
+    mosaic_data: Dict[str, Union[np.ndarray, list]],
+    a_batch: Optional[np.ndarray],
+    params: dict,
+    expected: Optional[dict],
 ):
     x_batch, y_batch, p_batch = (
         mosaic_data["x_batch"],
         mosaic_data["y_batch"],
-        mosaic_data["p_batch"]
+        mosaic_data["p_batch"],
     )
     metric = Focus(**params)
 
