3ce32e50ef2c739272e360c0a919b43b5fe415eb
Artem Sereda <Aitem1503@icloud.com>
2022-08-12 21:14:43 +0200

    add all tests

--- quantus/metrics/robustness_metrics/relative_stability.py
+++ quantus/metrics/robustness_metrics/relative_stability.py
@@ -16,13 +16,13 @@ from tqdm import tqdm
 
 @functools.partial(jax.jit, static_argnums=(4, 5, 6))
 def relative_stability_objective(
-        x: jnp.ndarray,
-        x_s: jnp.ndarray,
-        a: jnp.ndarray,
-        a_s: jnp.ndarray,
-        eps_min: float,
-        division_in_denominator: bool,
-        denominator_norm_axis: Union[Tuple[int, int], int]
+    x: jnp.ndarray,
+    x_s: jnp.ndarray,
+    a: jnp.ndarray,
+    a_s: jnp.ndarray,
+    eps_min: float,
+    division_in_denominator: bool,
+    denominator_norm_axis: Union[Tuple[int, int], int],
 ) -> jnp.ndarray:
     """
     Parameters
@@ -60,8 +60,7 @@ def relative_stability_objective(
 
 
 relative_stability_objective_vectorized_over_perturbation_axis = jax.vmap(
-    relative_stability_objective,
-    in_axes=(None, 0, None, 0, None, None, None)
+    relative_stability_objective, in_axes=(None, 0, None, 0, None, None, None)
 )
 
 
@@ -82,15 +81,12 @@ class RelativeStability(Metric, ABC):
         super().__init__(*args, **kwargs)
 
         self.eps_min = kwargs.pop("eps_min", self.DEFAULT_EPS_MIN)
-        self.num_perturbations = kwargs.pop("num_perturbations", self.DEFAULT_NUM_PERTURBATIONS)
+        self.num_perturbations = kwargs.pop(
+            "num_perturbations", self.DEFAULT_NUM_PERTURBATIONS
+        )
 
     def __call__(
-            self,
-            model,
-            x_batch: np.ndarray,
-            y_batch: np.ndarray,
-            *args,
-            **kwargs
+        self, model, x_batch: np.ndarray, y_batch: np.ndarray, *args, **kwargs
     ) -> Union[float, np.ndarray]:
 
         """
@@ -121,6 +117,11 @@ class RelativeStability(Metric, ABC):
         channel_first = utils.infer_channel_first(x_batch)
         self.model = utils.get_wrapped_model(model, channel_first)
 
+        if "a_batch" in kwargs and "as_batch" in kwargs and "xs_batch" not in kwargs:
+            raise ValueError(
+                "When providing pre-computed explanations, must also provide x' (xs_batch)"
+            )
+
         xs_batch, kwargs = self._get_perturbed_inputs(x_batch, y_batch, **kwargs)
         ex, exs, kwargs = self._get_explanations(x_batch, y_batch, xs_batch, **kwargs)
 
@@ -136,17 +137,21 @@ class RelativeStability(Metric, ABC):
 
         return result
 
-    def _get_perturbed_inputs(self, x_batch, y_batch, **kwargs) -> Tuple[np.ndarray, Dict]:
+    def _get_perturbed_inputs(
+        self, x_batch, y_batch, **kwargs
+    ) -> Tuple[np.ndarray, Dict]:
         """
         Returns:
             xs_batch: of perturbed inputs
             kwargs: Dict of updated kwargs
         """
 
-        if 'xs_batch' not in kwargs:
+        if "xs_batch" not in kwargs:
 
-            if 'perturb_func' not in kwargs:
-                warnings.warn('No "perturb_func" provided, using random noise as default')
+            if "perturb_func" not in kwargs:
+                warnings.warn(
+                    'No "perturb_func" provided, using random noise as default'
+                )
 
             perturb_function: Callable = kwargs.pop(
                 "perturb_func", perturb_func.random_noise
@@ -168,32 +173,42 @@ class RelativeStability(Metric, ABC):
                 xs_batch.append(xs)
 
             # pull all new images into 0 axes
-            xs_batch = np.asarray(xs_batch).reshape(-1, *x_batch.shape[1:])
+            xs_batch = np.vstack(xs_batch)
             # drop images, which cause dims not to be divisible
-            xs_batch = xs_batch[: xs_batch.shape[0] // x_batch.shape[0] * x_batch.shape[0]]
+            xs_batch = xs_batch[
+                : xs_batch.shape[0] // x_batch.shape[0] * x_batch.shape[0]
+            ]
             # make xs_batch have the same shape as x_batch, with new batching axis at 0
             xs_batch = xs_batch.reshape(-1, *x_batch.shape)
             return xs_batch, kwargs
 
-        xs_batch = kwargs.pop('xs_batch')
+        xs_batch = kwargs.pop("xs_batch")
         if len(xs_batch.shape) <= len(x_batch.shape):
-            raise ValueError('xs_batch must have 1 more batch axis than x_batch')
+            raise ValueError("xs_batch must have 1 more batch axis than x_batch")
 
         return xs_batch, kwargs
 
-    def _get_explanations(self, x_batch, y_batch, xs_batch, **kwargs) -> Tuple[np.ndarray, np.ndarray, Dict]:
+    def _get_explanations(
+        self, x_batch, y_batch, xs_batch, **kwargs
+    ) -> Tuple[np.ndarray, np.ndarray, Dict]:
         """
-                Returns:
-                    a_batch: batch of explanations
-                    as_batch: batch of batches of perturbed explanations
-                    kwargs: Dict of updated kwargs
+        Returns:
+            a_batch: batch of explanations
+            as_batch: batch of batches of perturbed explanations
+            kwargs: Dict of updated kwargs
         """
-        if ('explain_func' in kwargs and ('a_batch' in kwargs or 'as_batch' in kwargs)
-        ) or ('explain_func' not in kwargs and ('a_batch' not in kwargs or 'as_batch' not in kwargs)):
-            raise ValueError('Must provide either explain_func or (a_batch and as_batch)')
+        if (
+            "explain_func" in kwargs and ("a_batch" in kwargs or "as_batch" in kwargs)
+        ) or (
+            "explain_func" not in kwargs
+            and ("a_batch" not in kwargs or "as_batch" not in kwargs)
+        ):
+            raise ValueError(
+                "Must provide either explain_func or (a_batch and as_batch)"
+            )
 
-        if 'explain_func' in kwargs:
-            explain_func: Callable = kwargs.pop('explain_func')
+        if "explain_func" in kwargs:
+            explain_func: Callable = kwargs.pop("explain_func")
 
             a_batch = explain_func(
                 model=self.model.get_model(), inputs=x_batch, targets=y_batch, **kwargs
@@ -201,18 +216,23 @@ class RelativeStability(Metric, ABC):
 
             it = xs_batch
             if self.display_progressbar:
-                it = tqdm(it, desc=f'Collecting explanations for {self.name}')
+                it = tqdm(it, desc=f"Collecting explanations for {self.name}")
 
             as_batch = [
-                explain_func(model=self.model.get_model(), inputs=i, targets=y_batch, **kwargs) for i in it
+                explain_func(
+                    model=self.model.get_model(), inputs=i, targets=y_batch, **kwargs
+                )
+                for i in it
             ]
 
         else:
-            a_batch = kwargs.pop('a_batch')
-            as_batch = kwargs.pop('as_batch')
+            a_batch = kwargs.pop("a_batch")
+            as_batch = kwargs.pop("as_batch")
 
             if len(as_batch.shape) <= len(a_batch.shape):
-                raise ValueError('Batch of perturbed explanations must have 1 more axis')
+                raise ValueError(
+                    "Batch of perturbed explanations must have 1 more axis"
+                )
 
         if self.normalise:
             a_batch = self.normalise_func(a_batch)
@@ -224,7 +244,6 @@ class RelativeStability(Metric, ABC):
 
         return a_batch, np.asarray(as_batch), kwargs
 
-
     @abstractmethod
     def _compute_objective(self, x, xs, e, es) -> np.ndarray:
         """
@@ -234,7 +253,7 @@ class RelativeStability(Metric, ABC):
 
 
 class RelativeInputStability(RelativeStability):
-    name = 'Relative Input Stability'
+    name = "Relative Input Stability"
 
     """
     RIS(x, x', ex, ex') = max \frac{||\frac{e_x - e_{x'}}{e_x}||_p}{max (||\frac{x - x'}{x}||_p, \epsilon_{min})}
@@ -247,13 +266,20 @@ class RelativeInputStability(RelativeStability):
     """
 
     def _compute_objective(self, x, xs, e, es) -> np.ndarray:
-        result = relative_stability_objective_vectorized_over_perturbation_axis(x, xs, e, es, self.eps_min, True, (1, 2))
-        return result.to_py() # noqa
-
+        result = relative_stability_objective_vectorized_over_perturbation_axis(
+            jnp.asarray(x),
+            jnp.asarray(xs),
+            jnp.asarray(e),
+            jnp.asarray(es),
+            self.eps_min,
+            True,
+            (1, 2),
+        )
+        return result.to_py()  # noqa
 
 
 class RelativeOutputStability(RelativeStability):
-    name = 'Relative Output Stability'
+    name = "Relative Output Stability"
 
     """
     ROS(x, x', ex, ex') = max \frac{||\frac{e_x - e_{x'}}{e_x}||_p}{max (||\frac{h(x) - h(x')}||_p, \epsilon_{min})}
@@ -263,23 +289,37 @@ class RelativeOutputStability(RelativeStability):
     def _compute_objective(self, x, xs, e, es) -> np.ndarray:
         hx = self.model.predict(x)
         hxs = self.model.predict(xs)
-        result = relative_stability_objective_vectorized_over_perturbation_axis(hx, hxs, e, es, self.eps_min, False, 1)
-        return result.to_py() # noqa
-
+        result = relative_stability_objective_vectorized_over_perturbation_axis(
+            jnp.asarray(hx),
+            jnp.asarray(hxs),
+            jnp.asarray(e),
+            jnp.asarray(es),
+            self.eps_min,
+            False,
+            1,
+        )
+        return result.to_py()  # noqa
 
 
 class RelativeRepresentationStability(RelativeStability):
-    name = 'Relative Representation Stability'
+    name = "Relative Representation Stability"
 
     """
     RRS(x, x', ex, ex') = max \frac{||\frac{e_x - e_{x'}}{e_x}||_p}{max (||\frac{L_x - L_{x'}}{L_x}||_p, \epsilon_{min})}
        where L(·) denotes the internal model representation, e.g., output embeddings of hidden layers.
     """
 
-
     def _compute_objective(self, x, xs, e, es) -> np.ndarray:
         lx = self.model.get_hidden_layers_outputs(x)
         lxs = [self.model.get_hidden_layers_outputs(i) for i in xs]
         lxs = jnp.asarray(lxs)
-        result = relative_stability_objective_vectorized_over_perturbation_axis(lx, lxs, e, es, self.eps_min, True, 1)
-        return result.to_py() # noqa
+        result = relative_stability_objective_vectorized_over_perturbation_axis(
+            jnp.asarray(lx),
+            jnp.asarray(lxs),
+            jnp.asarray(e),
+            jnp.asarray(es),
+            self.eps_min,
+            True,
+            1,
+        )
+        return result.to_py()  # noqa
--- tests/metrics/test_relative_stability.py
+++ tests/metrics/test_relative_stability.py
@@ -5,9 +5,8 @@ from ..fixtures import *
 import quantus
 from quantus import relative_stability_objective
 import jax.numpy as jnp
-import pytest
 
-'''
+"""
 Following scenarios are to be tested for each Relative Stability metric
 
     - Test output shapes of relative stability objectives, which are to maximize (and if they do not fail on expected input shapes)
@@ -35,16 +34,16 @@ There are no desired values, the tests are rather just to make sure no exception
 
 Since all 3 relative stabilities are exactly the same, except for arguments provided to objective, 
 it's enough just to test 1 class extensively
-'''
+"""
 
 
 @pytest.mark.robustness
-def test_relative_stability_objective():
+def test_relative_input_stability_objective():
     x = np.random.random((5, 28, 28, 1))
 
     res = relative_stability_objective(x, x, x, x, 0.00001, True, (1, 2))
 
-    assert res.shape == (5,), 'Must output same dimension as inputs batch axis'
+    assert res.shape == (5,), "Must output same dimension as inputs batch axis"
 
 
 @pytest.mark.robustness
@@ -54,158 +53,330 @@ def test_relative_output_stability_objective():
 
     res = relative_stability_objective(h, h, a, a, 0.00001, False, 1)
 
-    assert res.shape == (5,), 'Must output same dimension as inputs batch axis'
+    assert res.shape == (5,), "Must output same dimension as inputs batch axis"
 
 
 @pytest.mark.robustness
-@pytest.mark.parametrize(
-    'model',
-    [
-        (
-                lazy_fixture('load_mnist_model_tf')
-        )
-    ]
-)
+@pytest.mark.parametrize("model", [(lazy_fixture("load_mnist_model_tf"))])
 def test_relative_representation_stability_objective(model):
-
     tf_model = quantus.utils.get_wrapped_model(model, False)
     x = np.random.random((5, 28, 28, 1))
     a = np.random.random((5, 28, 28, 1))
 
     lx = tf_model.get_hidden_layers_outputs(x)
 
-
     res = relative_stability_objective(lx, lx, a, a, 0.00001, True, 1)
 
-    assert res.shape == (5,), 'Must output same dimension as inputs batch axis'
+    assert res.shape == (5,), "Must output same dimension as inputs batch axis"
 
 
 @pytest.mark.robustness
 @pytest.mark.parametrize(
-    'model,data,params',
+    "model,data,params",
     [
-        (  # no explain func, no pre computed explanations
-                lazy_fixture('load_mnist_model_tf'),
-                lazy_fixture('load_mnist_images_tf'),
-                {}
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            # no explain func, no pre computed explanations
+            {},
         ),
         (
-                lazy_fixture('load_mnist_model_tf'),
-                lazy_fixture('load_mnist_images_tf'),
-                {
-                    # pre-computed perturbations don't have extra batch dimension
-                    'xs_batch': np.random.random((124, 28, 28, 1))
-                }
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            # pre-computed perturbations don't have extra batch dimension
+            {"xs_batch": np.random.random((124, 28, 28, 1))},
         ),
-        (  # only a_batch given
-                lazy_fixture('load_mnist_model_tf'),
-                lazy_fixture('load_mnist_images_tf'),
-                {
-                    'a_batch': np.random.random((124, 28, 28, 1))
-                }
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            # only a_batch given
+            {"a_batch": np.random.random((124, 28, 28, 1))},
         ),
-        (  # only as_batch given
-                lazy_fixture('load_mnist_model_tf'),
-                lazy_fixture('load_mnist_images_tf'),
-                {
-                    'as_batch': np.random.random((5, 124, 28, 28, 1))
-                }
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            # only as_batch given
+            {"as_batch": np.random.random((5, 124, 28, 28, 1))},
         ),
-        (  # a.batch.shape == as_batch.shape
-                lazy_fixture('load_mnist_model_tf'),
-                lazy_fixture('load_mnist_images_tf'),
-                {
-                    'a_batch': np.random.random((124, 28, 28, 1)),
-                    'as_batch': np.random.random((124, 28, 28, 1))
-                }
-        )
-    ]
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            # pre-computed perturbed explanations have no extra batch axis
+            {
+                "a_batch": np.random.random((124, 28, 28, 1)),
+                "as_batch": np.random.random((124, 28, 28, 1)),
+            },
+        ),
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            # provided pre-computed perturbed explanations, but not perturbed x
+            {
+                "a_batch": np.random.random((124, 28, 28, 1)),
+                "as_batch": np.random.random((5, 124, 28, 28, 1)),
+            },
+        ),
+    ],
+    ids=[
+        "no explain func, no pre computed explanations",
+        "pre-computed perturbations don't have extra batch dimension",
+        "only a_batch given",
+        "only as_batch given",
+        "pre-computed perturbed explanations have no extra batch axis",
+        "provided pre-computed perturbed explanations, but not perturbed x",
+    ],
 )
-def test_invalid_kwargs(
-        model, data, params
-):
+def test_invalid_kwargs(model, data, params):
     with pytest.raises(ValueError):
         ris = quantus.RelativeInputStability(**params)
-        ris(model, data['x_batch'], data['y_batch'], **params)
+        ris(model, data["x_batch"], data["y_batch"], **params)
 
 
 @pytest.mark.robustness
 @pytest.mark.parametrize(
-    'model,data,params',
+    "model,data,params",
     [
         (
-                lazy_fixture('load_mnist_model_tf'),
-                lazy_fixture('load_mnist_images_tf'),
-                {
-                    'explain_func': quantus.explain,
-                }
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {
+                "explain_func": quantus.explain,
+            },
         )
-    ]
+    ],
 )
-def test_relative_input_stability_pre_computed_perturbations(model, data, params):
+def test_pre_computed_perturbations(model, data, params):
     ris = quantus.RelativeInputStability(**params)
-    x = data['x_batch']
+    x = data["x_batch"]
     xs = np.asarray([quantus.random_noise(x) for _ in range(5)])
 
-    result = ris(model, x, data['y_batch'], xs_batch=xs, **params)
-    print(f'{result = }')
+    result = ris(model, x, data["y_batch"], xs_batch=xs, **params)
+    print(f"{result = }")
 
-    assert (result != jnp.nan).all(), 'Probably divided by 0'
+    assert (result != jnp.nan).all(), "Probably divided by 0"
 
 
 @pytest.mark.robustness
 @pytest.mark.parametrize(
-    'model,data,params',
+    "model,data,params",
     [
         (
-                lazy_fixture('load_mnist_model_tf'),
-                lazy_fixture('load_mnist_images_tf'),
-                {
-                    'explain_func': quantus.explain,
-                }
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {
+                "explain_func": quantus.explain,
+                "num_perturbations": 10,
+            },
         ),
         (
-                lazy_fixture('load_mnist_model_tf'),
-                lazy_fixture('load_mnist_images_tf'),
-                {
-                    'explain_func': quantus.explain,
-                    'perturb_func': quantus.gaussian_noise,
-                    'indices': list(range(124)),
-                    'indexed_axes': [0],
-                    'perturb_std': 0.5,
-                    'perturb_mean': 0.3
-                }
-        )
-    ]
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {
+                "explain_func": quantus.explain,
+                "num_perturbations": 10,
+                "perturb_func": quantus.gaussian_noise,
+                "indices": list(range(124)),
+                "indexed_axes": [0],
+                "perturb_std": 0.5,
+                "perturb_mean": 0.3,
+            },
+        ),
+    ],
+    ids=[
+        "default perturb_func",
+        "perturb_func = quantus.gaussian_noise, with extra kwargs",
+    ],
 )
-def test_relative_input_stability_compute_perturbations(model, data, params):
+def test_compute_perturbations(model, data, params):
     ris = quantus.RelativeInputStability(**params)
-    x = data['x_batch']
+    x = data["x_batch"]
 
-    result = ris(model, x, data['y_batch'], **params)
-    print(f'{result = }')
+    result = ris(model, x, data["y_batch"], **params)
+    print(f"{result = }")
 
-    assert (result != jnp.nan).all(), 'Probably divided by 0'
-
-
-@pytest.mark.robustness
-def test_relative_input_stability_precomputed_explanations():
-    pass
-
+    assert (result != jnp.nan).all(), "Probably divided by 0"
 
 
 @pytest.mark.robustness
-def test_relative_input_stability_compute_explanations():
-    pass
+@pytest.mark.parametrize(
+    "model,data,params",
+    [(lazy_fixture("load_mnist_model_tf"), lazy_fixture("load_mnist_images_tf"), {})],
+)
+def test_precomputed_explanations(model, data, params):
+    x = data["x_batch"]
+    ex = quantus.explain(model, x, data["y_batch"])
+
+    ris = quantus.RelativeInputStability(**params)
+    result = ris(
+        model,
+        x,
+        data["y_batch"],
+        xs_batch=np.stack([x, x]),
+        a_batch=ex,
+        as_batch=np.stack([ex, ex]),
+    )
+
+    print(f"{result = }")
+
+    assert (result != jnp.nan).all(), "Probably divided by 0"
 
 
 @pytest.mark.robustness
-# @pytest.mark.parametrize()
-def test_relative_output_stability():
-    pass
+@pytest.mark.parametrize(
+    "model,data,params",
+    [
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {
+                "explain_func": quantus.explain,
+                "method": "Gradient",
+                "num_perturbations": 10,
+            },
+        ),
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {
+                "explain_func": quantus.explain,
+                "method": "IntegratedGradients",
+                "num_perturbations": 10,
+            },
+        ),
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {
+                "explain_func": quantus.explain,
+                "method": "InputXGradient",
+                "num_perturbations": 10,
+            },
+        ),
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {
+                "explain_func": quantus.explain,
+                "method": "Occlusion",
+                "num_perturbations": 10,
+            },
+        ),
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {
+                "explain_func": quantus.explain,
+                "method": "GradCam",
+                "num_perturbations": 10,
+            },
+        ),
+    ],
+    ids=[
+        "method = Gradient",
+        "method = IntegratedGradients",
+        "method = InputXGradient",
+        "method = Occlusion",
+        "method = GradCam",
+    ],
+)
+def test_compute_explanations(model, data, params):
+    print(f"{params['method'] = }")
+    ris = quantus.RelativeInputStability(**params)
+
+    result = ris(model, data["x_batch"], data["y_batch"], **params)
+    print(f"{result = }")
+
+    assert (result != jnp.nan).all(), "Probably divided by 0"
 
 
 @pytest.mark.robustness
-# @pytest.mark.parametrize()
-def test_relative_representation_stability():
-    pass
+@pytest.mark.parametrize(
+    "model,data,params",
+    [
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {"explain_func": quantus.explain, "num_perturbations": 10, "abs": True},
+        ),
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {
+                "explain_func": quantus.explain,
+                "num_perturbations": 10,
+                "normalise": True,
+            },
+        ),
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {
+                "explain_func": quantus.explain,
+                "num_perturbations": 10,
+                "display_progressbar": True,
+            },
+        ),
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {
+                "explain_func": quantus.explain,
+                "num_perturbations": 10,
+                "return_aggregate": True,
+            },
+        ),
+    ],
+    ids=[
+        "abs = True",
+        "normalise = True",
+        "display_progressbar = True",
+        "return_aggregate = True",
+    ],
+)
+def test_params_to_base_class(model, data, params):
+    ris = quantus.RelativeInputStability(**params)
+
+    result = ris(model, data["x_batch"], data["y_batch"], **params)
+    print(f"{result = }")
+
+    assert (result != jnp.nan).all(), "Probably divided by 0"
+
+
+@pytest.mark.robustness
+@pytest.mark.parametrize(
+    "model,data,params",
+    [
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {"explain_func": quantus.explain, "num_perturbations": 10},
+        ),
+    ],
+)
+def test_relative_output_stability(model, data, params):
+    ros = quantus.RelativeOutputStability(**params)
+
+    result = ros(model, data["x_batch"], data["y_batch"], **params)
+    print(f"{result = }")
+
+    assert (result != jnp.nan).all(), "Probably divided by 0"
+
+
+@pytest.mark.robustness
+@pytest.mark.parametrize(
+    "model,data,params",
+    [
+        (
+            lazy_fixture("load_mnist_model_tf"),
+            lazy_fixture("load_mnist_images_tf"),
+            {"explain_func": quantus.explain, "num_perturbations": 10},
+        ),
+    ],
+)
+def test_relative_representation_stability(model, data, params):
+    rrs = quantus.RelativeRepresentationStability(**params)
+
+    result = rrs(model, data["x_batch"], data["y_batch"], **params)
+    print(f"{result = }")
+
+    assert (result != jnp.nan).all(), "Probably divided by 0"
