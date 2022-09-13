8073f8989216beb86e8f1220fa93e4ff9c5d535e
annahedstroem <anna.hedstroem@tu-berlin.de>
2022-09-13 21:59:25 +0200

    Faithfulness metrics fixed

--- quantus/metrics/__init__.py
+++ quantus/metrics/__init__.py
@@ -1,8 +1,7 @@
 from .base import *
 from .axiomatic import *
 from .complexity import *
+from .faithfulness import *
 from .localisation import *
 from .randomisation import *
-
-from .robustness_metrics import *
-from .faithfulness_metrics import *
+from .robustness import *
