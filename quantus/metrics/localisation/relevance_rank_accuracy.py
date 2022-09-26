93e59b4d54a60a9a9d621c1ced87a4db295cb01f
Anna Hedström <anna.hedstroem@tu-berlin.de>
2022-09-26 14:02:47 +0200

    Remove old #TODO

--- quantus/metrics/localisation/relevance_rank_accuracy.py
+++ quantus/metrics/localisation/relevance_rank_accuracy.py
@@ -128,7 +128,6 @@ class RelevanceRankAccuracy(Metric):
         c: Any,
         p: Any,
     ) -> float:
-        # TODO. Fixme, returns 3.985969387755102e-05.
 
         # Return np.nan as result if segmentation map is empty.
         if np.sum(s) == 0:
