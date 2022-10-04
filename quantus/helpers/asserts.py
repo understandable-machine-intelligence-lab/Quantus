9f27d9b9e0afcd4caa2fc94eb19aa6b3fb93b9c4
annahedstroem <anna.hedstroem@tu-berlin.de>
2022-10-04 19:05:14 +0200

    Added license stubs to src files

--- quantus/helpers/asserts.py
+++ quantus/helpers/asserts.py
@@ -1,8 +1,12 @@
 """This module holds a collection of asserts functionality that is used across the Quantus library to avoid undefined behaviour."""
 
+# This file is part of Quantus.
+# Quantus is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# Quantus is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public License along with Quantus. If not, see <https://www.gnu.org/licenses/>.
+
 import numpy as np
 from typing import Callable, Tuple, Union, Sequence
-
 
 def attributes_check(metric):
     """
--- quantus/metrics/randomisation/model_parameter_randomisation.py
+++ quantus/metrics/randomisation/model_parameter_randomisation.py
@@ -1,5 +1,10 @@
 """This module contains the implementation of the Model Parameter Sensitivity metric."""
 
+# This file is part of Quantus.
+# Quantus is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# Quantus is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public License along with Quantus. If not, see <https://www.gnu.org/licenses/>.
+
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 import numpy as np
 from tqdm.auto import tqdm
