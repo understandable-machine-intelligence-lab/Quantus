57dd36a4b02e5d8e989454900f6a0cc4f5b3a77d
annahedstroem <anna.hedstroem@tu-berlin.de>
2022-10-08 20:25:05 +0200

    Updated conf tag

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = "Quantus"
 copyright = f"{str(datetime.utcnow().year)}, Anna Hedström"
 author = 'Anna Hedström'
-release = 'v0.2.1'
+release = 'v0.2.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
