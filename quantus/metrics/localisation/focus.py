6f541b2b34c39d643205fdc6bc67118f410bbee1
Daniel Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2022-10-12 21:38:33 +0200

    Remove all remaining occurences in docstrings for unused c and p arguments.
    https://github.com/understandable-machine-intelligence-lab/Quantus/pull/167#discussion_r993579809

--- quantus/metrics/localisation/focus.py
+++ quantus/metrics/localisation/focus.py
@@ -249,10 +249,10 @@ class Focus(Metric):
         i: int,
         model: ModelInterface,
         x: np.ndarray,
-        y: np.ndarray,
-        a: np.ndarray,
-        s: np.ndarray,
-        c: np.ndarray,
+        y: np.ndarray = None,
+        a: np.ndarray = None,
+        s: np.ndarray = None,
+        c: np.ndarray = None,
     ) -> float:
         """
         Evaluate instance gets model and data for a single instance as input and returns the evaluation result.
--- quantus/metrics/randomisation/model_parameter_randomisation.py
+++ quantus/metrics/randomisation/model_parameter_randomisation.py
@@ -317,10 +317,10 @@ class ModelParameterRandomisation(Metric):
         self,
         model: ModelInterface,
         x: np.ndarray,
-        y: np.ndarray,
-        a: np.ndarray,
-        s: np.ndarray,
-        a_perturbed: np.ndarray,
+        y: np.ndarray = None,
+        a: np.ndarray = None,
+        s: np.ndarray = None,
+        a_perturbed: np.ndarray = None,
     ) -> float:
         """
         Evaluate instance gets model and data for a single instance as input and returns the evaluation result.
@@ -339,10 +339,8 @@ class ModelParameterRandomisation(Metric):
             The explanation to be evaluated on an instance-basis.
         s: np.ndarray
             The segmentation to be evaluated on an instance-basis.
-        c: any
-            The custom input to be evaluated on an instance-basis.
-        p: any
-            The custom preprocess input to be evaluated on an instance-basis.
+        a_perturbed: np.ndarray
+            The perturbed attributions.
 
         Returns
         -------
