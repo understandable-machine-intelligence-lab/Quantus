be30428ed3f7991cb80ba75a38060baca3cd6fdd
Daniel Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2022-10-12 22:16:58 +0200

    Remove custom_batch from __init__ signatures in metrics which don't use this attribute.

--- quantus/metrics/robustness/max_sensitivity.py
+++ quantus/metrics/robustness/max_sensitivity.py
@@ -164,7 +164,6 @@ class MaxSensitivity(PerturbationMetric):
         y_batch: np.array,
         a_batch: Optional[np.ndarray] = None,
         s_batch: Optional[np.ndarray] = None,
-        custom_batch: Optional[np.ndarray] = None,
         channel_first: Optional[bool] = None,
         explain_func: Optional[Callable] = None,
         explain_func_kwargs: Optional[Dict[str, Any]] = None,
@@ -208,9 +207,6 @@ class MaxSensitivity(PerturbationMetric):
             This is used for this __call__ only and won't be saved as attribute. If None, self.softmax is used.
         device: string
             Indicated the device on which a torch.Tensor is or will be allocated: "cpu" or "gpu".
-        custom_batch: any
-            Any object that can be passed to the evaluation process.
-            Gives flexibility to the user to adapt for implementing their own metric.
         kwargs: optional
             Keyword arguments.
 
@@ -255,7 +251,7 @@ class MaxSensitivity(PerturbationMetric):
             y_batch=y_batch,
             a_batch=a_batch,
             s_batch=s_batch,
-            custom_batch=custom_batch,
+            custom_batch=None,
             channel_first=channel_first,
             explain_func=explain_func,
             explain_func_kwargs=explain_func_kwargs,
