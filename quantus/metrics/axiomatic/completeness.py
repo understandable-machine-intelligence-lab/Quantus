0c88ac226d34763894238bd5c5b508ba57ce9bb2
Daniel Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2022-10-12 15:52:49 +0200

    apply PertubationMetric evaluate_instance() signate changes to all PerturbationMetrics

--- quantus/metrics/axiomatic/completeness.py
+++ quantus/metrics/axiomatic/completeness.py
@@ -146,6 +146,7 @@ class Completeness(PerturbationMetric):
         y_batch: np.array,
         a_batch: Optional[np.ndarray] = None,
         s_batch: Optional[np.ndarray] = None,
+        custom_batch: Optional[np.ndarray] = None,
         channel_first: Optional[bool] = None,
         explain_func: Optional[Callable] = None,
         explain_func_kwargs: Optional[Dict[str, Any]] = None,
@@ -189,6 +190,9 @@ class Completeness(PerturbationMetric):
             This is used for this __call__ only and won't be saved as attribute. If None, self.softmax is used.
         device: string
             Indicated the device on which a torch.Tensor is or will be allocated: "cpu" or "gpu".
+        custom_batch: any
+            Any object that can be passed to the evaluation process.
+            Gives flexibility to the user to adapt for implementing their own metric.
         kwargs: optional
             Keyword arguments.
 
@@ -233,7 +237,7 @@ class Completeness(PerturbationMetric):
             y_batch=y_batch,
             a_batch=a_batch,
             s_batch=s_batch,
-            custom_batch=None,
+            custom_batch=custom_batch,
             channel_first=channel_first,
             explain_func=explain_func,
             explain_func_kwargs=explain_func_kwargs,
@@ -245,6 +249,7 @@ class Completeness(PerturbationMetric):
 
     def evaluate_instance(
         self,
+        i: int,
         model: ModelInterface,
         x: np.ndarray,
         y: np.ndarray = None,
@@ -258,6 +263,8 @@ class Completeness(PerturbationMetric):
 
         Parameters
         ----------
+        i: integer
+            The evaluation instance.
         model: ModelInterface
             A ModelInteface that is subject to explanation.
         x: np.ndarray
