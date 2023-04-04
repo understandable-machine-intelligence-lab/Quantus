f367a45cef9e3267747c8432ee6123effcd938c2
aaarrti <artem.sereda@campus.tu-berlin.de>
2023-04-04 20:49:12 +0200

    resolve conflicts

--- tests/models.py
+++ tests/models.py
@@ -218,3 +218,22 @@ def TitanicSimpleTFModel() -> tf.keras.Model:
             tf.keras.layers.Dense(2, activation="softmax"),
         ]
     )
+
+
+def CifarCNNModel() -> tf.keras.Model:
+    """
+    Source: https://www.tensorflow.org/tutorials/images/cnn
+    """
+
+    return tf.keras.Sequential(
+        [
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(10),
+        ]
+    )
