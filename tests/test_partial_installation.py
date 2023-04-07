20bc2dd0e288df5c0e69c55ba84136c564d77d99
aaarrti <artem.sereda@campus.tu-berlin.de>
2023-04-07 02:47:07 +0200

    *

--- tests/test_partial_installation.py
+++ tests/test_partial_installation.py
@@ -1,21 +1,31 @@
 import pytest
+from quantus.helpers.tf_utils import is_tf_available
+from quantus.helpers.torch_utils import is_torch_available
+
+skip_if_tf_and_torch_available = pytest.mark.skipif(
+    is_tf_available() and is_torch_available()
+)
 
 
 @pytest.mark.partial_installation
+@skip_if_tf_and_torch_available
 def test_only_tensorflow_installed():
     import quantus
 
 
 @pytest.mark.partial_installation
+@skip_if_tf_and_torch_available
 def test_only_torch_installed():
-    import transformers
+    import quantus
 
 
 @pytest.mark.partial_installation
+@skip_if_tf_and_torch_available
 def test_transformers_with_torch_installed():
-    import tensorflow
+    import quantus
 
 
 @pytest.mark.partial_installation
+@skip_if_tf_and_torch_available
 def test_transformers_with_tensorflow_installed():
-    import tensorflow
+    import quantus
