bead7b4e84b174aaf40a9d4764640c27a5882f69
annahedstroem <anna.hedstroem@tu-berlin.de>
2023-06-23 12:23:42 +0200

    ran black

--- quantus/functions/loss_func.py
+++ quantus/functions/loss_func.py
@@ -34,7 +34,7 @@ def mse(a: np.array, b: np.array, **kwargs) -> float:
 
     if normalise:
         # Calculate MSE in its polynomial expansion (a-b)^2 = a^2 - 2ab + b^2.
-        return np.average(((a**2) - (2 * (a * b)) + (b**2)), axis=0)
+        return np.average(((a ** 2) - (2 * (a * b)) + (b ** 2)), axis=0)
     # If no need to normalise, return (a-b)^2.
 
     return np.average(((a - b) ** 2), axis=0)
