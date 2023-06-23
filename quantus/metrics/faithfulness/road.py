a33f42e8dc9c013a95460a9682b6b510c5174bfd
annahedstroem <anna.hedstroem@tu-berlin.de>
2023-06-23 11:47:22 +0200

    minor fixes, delete of github.release

--- quantus/metrics/faithfulness/road.py
+++ quantus/metrics/faithfulness/road.py
@@ -364,7 +364,6 @@ class ROAD(PerturbationMetric):
         """
 
         # Calculate accuracy for every number of most important pixels removed.
-
         self.last_results = {
             percentage: np.mean(np.array(self.last_results)[:, p_ix])
             for p_ix, percentage in enumerate(self.percentages)
